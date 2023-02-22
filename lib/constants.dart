7e5e66448de9afdcafe98e925ace058b863208a6
Rumaisha2222 <rumiashfaq2june@gmail.com>
2023-02-22 19:15:33 +0500

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color correct = const Color(0xFF27AD0C);
+Color incorrect = const Color(0xFFB12525);
+Color neutral = const Color(0xFFFFFFFF);
+Color background = const Color(0x00000000);
